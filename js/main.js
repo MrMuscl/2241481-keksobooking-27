97f852a179433023835564f42dda641ba5bf3587
Mr Muscl <anton.kurkov@gmail.com>
2022-12-04 01:37:14 +0300

    Implemented generation of mock data. Divided code on modules.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,3 @@
-function getRandomPositiveInteger(a, b){
-  if (typeof a !== 'number' || typeof b !== 'number' || a < 0 || b < 0){
-    return NaN;
-  }
+import {generateOffers} from './data.js';
 
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = (upper - lower) * Math.random() + lower;
-
-  return Math.floor(result);
-}
-
-function getRandomPositiveFloat(a, b, digits){
-  if (typeof a !== 'number' ||
-      typeof b !== 'number' ||
-      typeof digits !== 'number' ||
-      a < 0 || b < 0 || digits < 0){
-    return NaN;
-  }
-
-  const lower = Math.min(a, b);
-  const upper = Math.max(a, b);
-  const result = (upper - lower) * Math.random() + lower;
-
-  return +result.toFixed(digits);
-}
-
-getRandomPositiveFloat(1, 10, 6);
-getRandomPositiveInteger(2, 12);
+generateOffers();

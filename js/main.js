472119a2e5abba2922c302da41a469c0723dc89c
Mr Muscl <anton.kurkov@gmail.com>
2023-01-16 23:18:29 +0300

    Make forms disable in html markup.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 import {initAvatarAndPhotos} from './avatar.js';
-import {makeInactive} from './form.js';
 import {initMap} from './map.js';
 import {initSlider} from './slider.js';
 import {initValidation} from './validate.js';
 
-
-makeInactive();
 initValidation();
 initMap();
 initSlider();
